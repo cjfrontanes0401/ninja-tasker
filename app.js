ec02bb37e733f8fdbed81bd3068aeec1cdb0dbe0
cjfrontanes0401 <r3doppz@gmail.com>
2019-11-12 15:44:15 -0500

    Ninja tasker project done

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
+//packages
+const express = require("express");
+const bodyParser = require("body-parser");
+//starting express
+const app = express();
 
+//setting view engine
+app.set("view engine", "ejs");
+
+//middleware
+app.use(express.static("./public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+let list = ["Code and Gamer", "Slack line tonight"];
+
+// ROUTES
+//GET home
+app.get("/home", function(req, res) {
+  res.render("home.ejs", { list: list });
+});
+
+//POST /ninja
+app.post("/ninja", function(req, res) {
+  console.log(req.body.taskItem);
+  list.push(req.body.taskItem);
+  res.render("home.ejs", { list: list });
+});
+
+//server listening for request
+app.listen(3000, function() {
+  console.log("Server is Live!!!");
+});
